caa837d27a3b5f4ab0fae3c274e4e524711aae05
VyTrg <truongvy03.it@gmail.com>
2023-08-08 17:50:27 +0700

    All accepted

--- Repetitions.cpp
+++ Repetitions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,24 +8,31 @@ int Max(int a, int b);
 
 int main()
 {
-	string s;
-	cin >> s;
-	int n = size(s);
-	int k, h;
-	k = h = 1;
-	for(int i = 1; i < n; ++i)
+	int n, k;
+	int a[100];
+	cin >> n;
+	for(int i = 0; i < n; ++i)
 	{
-		if(s[i] == s[i - 1])
-		{
-			k++;
-		}
-		else
-		{
-			k = 1;
-		}
-		h = Max(k, h);
+		cin >> a[i];
 	}
-	cout << h;
+	int length, key;
+	length = key = 0;
+	for(int i = 0; i < n; ++i)
+	{
+		for(int j = i + 1; j < n; ++i)
+		{
+			if(a[j] != a[i])
+			{
+				length++;
+			}
+			else
+			{
+				length = 0;
+			}
+		}
+		key = Max(length, key);
+	}
+	cout << key;
 	return 0;
 }
 
